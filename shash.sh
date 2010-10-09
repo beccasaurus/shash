19fe94733e5bc0a17b2ddac6f71fed27ae73eea1
remi <remi@remitaylor.com>
2010-10-09 02:02:00 -0700

    little tweaks.  made some functions 'private'.  cleaned up some code.

--- shash.sh
+++ shash.sh
@@ -1,7 +1,7 @@
 # shash - Hash functionality for /bin/sh
 
 shash() {
-	hash="$1"; key="$2"; value="$3"
+	hash=$1; key=$2; value=$3
 
 	if [ -z "$key" ]; then
 		shash_keys_and_values "$hash"
@@ -16,57 +16,38 @@ shash_set() {
 	hash=$1; key=$2; value=$3
 
 	# set variable for this key
-	varname=`shash_variable_name_for_hash_and_key "$hash" "$key"`
+	varname=`__shash_variable_name_for_hash_and_key "$hash" "$key"`
 	eval "$varname='$value'"
 
 	# update the variable that stores the names of all keys for this hash
-	keys_varname=`shash_variable_name_for_hash_keys "$hash"`
+	keys_varname=`__shash_variable_name_for_hash_keys "$hash"`
 	eval "${keys_varname}=\"\${${keys_varname}}${key}\n\""
 }
 
 shash_get() {
 	hash=$1; key=$2
-	varname=`shash_variable_name_for_hash_and_key "$hash" "$key"`
+	varname=`__shash_variable_name_for_hash_and_key "$hash" "$key"`
 	eval "echo \$${varname}"
-}
-
-shash_variable_name_for_hash_keys() {
-	varname=`shash_safe_variable_name "$1"`
-	printf "__shash__${varname}__keys"
-}
-
-shash_variable_name_for_hash_and_key() {
-	hash=$1; key=$2
-	safe_hash=`shash_safe_variable_name "$hash"`
-	safe_key=` shash_safe_variable_name "$key"`
-	printf "__shash__${safe_hash}__${safe_key}"
-}
-
-shash_safe_variable_name() {
-	printf `printf "$1" | sed 's/[^[:alnum:]]//g'`
 }
 
 shash_keys() {
 	hash=$1
-	keys_varname=`shash_variable_name_for_hash_keys "$hash"`
+	keys_varname=`__shash_variable_name_for_hash_keys "$hash"`
 	eval "printf \"\$$keys_varname\""
 }
 
 shash_values() {
 	hash=$1
-	for key in `shash_keys "$hash"`; do
-		shash "$hash" "$key"
-	done
+	shash_echo "$hash" '$value'
 }
 
 shash_keys_and_values() {
-	hash="$1"
+	hash=$1
 	shash_echo "$hash" '$key: $value'
 }
 
 shash_each() {
-	hash=$1
-	code=$2
+	hash=$1; code=$2
 	for key in `shash_keys "$hash"`; do
 		value=`shash_get "$hash" "$key"`
 		eval "$code"
@@ -74,25 +55,25 @@ shash_each() {
 }
 
 shash_echo() {
-	hash=$1
-	code=$2
+	hash=$1; code=$2
 	shash_each "$hash" "echo \"$code\""
 }
 
 shash_delete() {
-	hash=$1
-	key=$2
+	hash=$1; key=$2
 
 	# unset the actual variable
-	hash_and_key_variable=`shash_variable_name_for_hash_and_key "$hash" "$key"`
+	hash_and_key_variable=`__shash_variable_name_for_hash_and_key "$hash" "$key"`
 	eval "unset $hash_and_key_variable"
 
 	# remove the key from our list of keys
-	keys_varname=`shash_variable_name_for_hash_keys "$hash"`
+	keys_varname=`__shash_variable_name_for_hash_keys "$hash"`
 	old_keys=`shash_keys "$hash"`
 	new_keys=`echo "$old_keys" | grep -v "^${key}$"`
 	eval "${keys_varname}=\"$new_keys\""
 }
+
+# Defining your own Hash helper function
 
 shash_define() {
 	hash=$1
@@ -117,3 +98,22 @@ eval "
 	}
 "
 }
+
+# PRIVATE
+
+__shash_variable_name_for_hash_keys() {
+	varname=`__shash_safe_variable_name "$1"`
+	printf "__shash__${varname}__keys"
+}
+
+__shash_variable_name_for_hash_and_key() {
+	hash=$1; key=$2
+	safe_hash=`__shash_safe_variable_name "$hash"`
+	safe_key=` __shash_safe_variable_name "$key"`
+	printf "__shash__${safe_hash}__${safe_key}"
+}
+
+__shash_safe_variable_name() {
+	printf `printf "$1" | sed 's/[^[:alnum:]]//g'`
+}
+
--- test-script.sh
+++ test-script.sh
@@ -5,4 +5,4 @@ shash_define dogs
 dogs Lander "American Pitbull Terrier"
 dogs Murdock "Australian Shepherd"
 
-dogs
+dogs_echo 'The dog named $key is a(n) $value'
